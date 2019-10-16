d9212607172371111f606930d762d117fe3e0c84
Charles <charles.bardin.pro@gmail.com>
2019-10-16 20:19:18 +0200

    implementing getMovieList method of MovieService

--- app/services/movie.js
+++ app/services/movie.js
@@ -1,4 +1,23 @@
 import Service from '@ember/service';
+import { set } from '@ember/object';
 
+const GHIBLI_API_MOVIE_BASE_URL = 'https://ghibliapi.herokuapp.com/films'
+
+// See https://guides.emberjs.com/release/tutorial/service/
 export default Service.extend({
+  init() {
+    if (!Array.isArray(this.movieList)) {
+      set(this, 'movieList', []);
+    }
+
+    this._super(...arguments);
+  },
+  async getMovieList() {
+    if (this.movieList.length === 0) {
+      const response = await fetch(GHIBLI_API_MOVIE_BASE_URL)
+      this.movieList = await response.json()
+    }
+
+    return this.movieList
+  }
 });
