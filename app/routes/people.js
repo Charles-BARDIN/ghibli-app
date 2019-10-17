a3dafd16eeb390c65f7410c3377c0d87dad67173
Charles Bardin <charles.bardin@pmu.fr>
2019-10-17 14:48:33 +0200

    get people on page load

--- app/routes/people.js
+++ app/routes/people.js
@@ -1,4 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service'
 
 export default Route.extend({
+  people: service(),
+  model(param) {
+    if (param.id == null) {
+      this.transitionTo('index');
+    }
+
+    return this.people.getByID(param.id)
+  }
 });
